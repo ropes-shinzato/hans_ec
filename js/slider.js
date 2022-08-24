2364ec3c167062c113ef533147cd65820021ab50
naoya-shinzato <naoya.shinzato@ropes.co.jp>
2022-08-24 19:50:35 +0900

    topページを制作

--- js/slider.js
+++ js/slider.js
@@ -24,18 +24,20 @@ $(function(){
 $(function(){    
     $('.slider__list02').slick({
         autoplay:true,//自動再生する
-        autoplaySpeed:4000,//自動再生するスピード 4秒
+        autoplaySpeed:400000,//自動再生するスピード 4秒
         dots:false,//ドット部分を表示する
-        centerMode: true,
-        centerPadding:'27.3%',
+        centerMode: false,
+        
         arrows:true,
-        slidesToShow: 1,
+        slidesToShow: 3,
         responsive: [
             {
               breakpoint: 768, // 399px以下のサイズに適用
               settings: {
                 slidesToShow: 1,
                 arrows:false,
+                centerMode: true,
+        centerPadding:'27.3%',
               },
             },
           ],
@@ -46,7 +48,7 @@ $(function(){
 $(function(){    
     $('.ranking__list').slick({
         autoplay:true,//自動再生する
-        autoplaySpeed:400000,//自動再生するスピード 4秒
+        autoplaySpeed:4000,//自動再生するスピード 4秒
         dots:false,//ドット部分を表示する
         centerMode: true,
         centerPadding:'25%',
