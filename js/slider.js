cc6d5d7742decc762f057baacf6a0995a2cc17a4
naoya-shinzato <naoya.shinzato@ropes.co.jp>
2022-08-26 13:01:49 +0900

    商品詳細ページを追加

--- js/slider.js
+++ js/slider.js
@@ -71,7 +71,7 @@ $(function(){
   
 });
 
-/* 商品詳細ページ */
+/* 商品詳細ページsp */
 $(function(){    
   $('.productd-details__img__list').slick({
       autoplay:true,//自動再生する
@@ -91,4 +91,48 @@ $(function(){
           },
         ],
   });
+});
+
+/* 商品詳細ページpc */
+$(function() {
+
+  $('#slider').slick({
+     /* asNavFor:"#thumbs", */
+ 
+     
+     dots: false,
+     autoplay: true,
+     autoplaySpeed: 3000,
+     speed: 1000,
+      infinite: true, 
+     slidesToShow: 1,
+     arrows: false,
+     
+ });
+ });
+
+/* 商品詳細ページpic-up*/
+$(function(){    
+  
+
+    $('.pickup__list').slick({
+        autoplay:true,//自動再生する
+        autoplaySpeed:4000,//自動再生するスピード 4秒
+        dots:false,//ドット部分を表示する
+        centerMode: true,
+        centerPadding:'25%',
+        arrows:true,
+        slidesToShow: 2,
+        responsive: [
+            {
+              breakpoint: 768, // 399px以下のサイズに適用
+              settings: {
+                slidesToShow: 1,
+                arrows:false,
+              },
+            },
+          ],
+    });
+  
+  
 });
